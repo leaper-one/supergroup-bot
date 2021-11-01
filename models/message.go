8c5570d63205ae9775b2f19ee087b529c8633b7e
Neo <her_liu@qq.com>
2021-11-01 09:06:27 +0800

    1. rename 2. add some comment

--- models/message.go
+++ models/message.go
@@ -2,12 +2,8 @@ package models
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
-	"net/url"
-	"strconv"
 	"time"
-	"unicode"
 
 	"github.com/MixinNetwork/supergroup/config"
 	"github.com/MixinNetwork/supergroup/durable"
@@ -15,8 +11,6 @@ import (
 	"github.com/MixinNetwork/supergroup/tools"
 	"github.com/fox-one/mixin-sdk-go"
 	"github.com/jackc/pgx/v4"
-	"github.com/shopspring/decimal"
-	"mvdan.cc/xurls"
 )
 
 const messages_DDL = `
@@ -180,7 +174,7 @@ func ReceivedMessage(ctx context.Context, clientID string, msg *mixin.MessageVie
 				return nil
 			}
 			go SendAssetsNotPassMsg(clientID, msg.UserID, msg.MessageID, false)
-			if checkCanSpeak(ctx, clientID, msg.UserID, ClientUserStatusAudience) {
+			if checkMessageCountLimit(ctx, clientID, msg.UserID, ClientUserStatusAudience) {
 				go SendToClientManager(clientID, msg, true, true)
 			}
 			return nil
@@ -194,7 +188,7 @@ func ReceivedMessage(ctx context.Context, clientID string, msg *mixin.MessageVie
 		fallthrough
 	// 大户
 	case ClientUserStatusLarge:
-		// 单独检测 禁止发的消息类型 这三种消息不能发。
+
 		if checkMsgIsForbid(clientUser, msg) {
 			return nil
 		}
@@ -262,8 +256,7 @@ func ReceivedMessage(ctx context.Context, clientID string, msg *mixin.MessageVie
 		fallthrough
 	// 嘉宾
 	case ClientUserStatusGuest:
-		// 检测是否达到每分钟发言上限
-		if !checkCanSpeak(ctx, clientID, msg.UserID, clientUser.Status) {
+		if !checkMessageCountLimit(ctx, clientID, msg.UserID, clientUser.Status) {
 			// 达到限制
 			go SendLimitMsg(clientID, msg.UserID, statusLimitMap[clientUser.Status])
 			return nil
@@ -301,29 +294,6 @@ func ReceivedMessage(ctx context.Context, clientID string, msg *mixin.MessageVie
 	return nil
 }
 
-func checkCanSpeak(ctx context.Context, clientID, userID string, status int) bool {
-	count := 0
-	if err := session.Database(ctx).QueryRow(ctx, `
-SELECT count(1) FROM messages 
-WHERE client_id=$1 
-AND user_id=$2 
-AND now()-created_at<interval '1 minutes'
-`, clientID, userID).Scan(&count); err != nil {
-		return false
-	}
-	limit := statusLimitMap[status]
-	return count < limit
-}
-
-func checkCategory(ctx context.Context, clientID, category string, status int) bool {
-	if category == mixin.MessageCategoryMessageRecall ||
-		status == ClientUserStatusAdmin ||
-		status == ClientUserStatusGuest {
-		return true
-	}
-	return checkHasClientMemberAuth(ctx, clientID, category, status)
-}
-
 func GetClientLastMsg(ctx context.Context) (map[string]time.Time, error) {
 	clients, err := getAllClient(ctx)
 	if err != nil {
@@ -342,180 +312,4 @@ func GetClientLastMsg(ctx context.Context) (map[string]time.Time, error) {
 		lms[client] = lm
 	}
 	return lms, nil
-}
-
-var cacheSendJoinMsg = make(map[string]time.Time)
-
-func checkIsSendJoinMsg(userID string) bool {
-	if cacheSendJoinMsg[userID].IsZero() {
-		cacheSendJoinMsg[userID] = time.Now()
-		return false
-	}
-	if cacheSendJoinMsg[userID].Add(time.Minute * 5).Before(time.Now()) {
-		cacheSendJoinMsg[userID] = time.Now()
-		return false
-	}
-	return true
-}
-
-func checkIsJustJoinGroup(u *ClientUser) bool {
-	return u.CreatedAt.Add(time.Minute * 5).After(time.Now())
-}
-func checkHasURLMsg(ctx context.Context, clientID string, msg *mixin.MessageView) bool {
-	hasURL := false
-	if msg.Category == mixin.MessageCategoryPlainImage {
-		if url, err := tools.MessageQRFilter(ctx, GetMixinClientByID(ctx, clientID).Client, msg); err == nil {
-			if url != "" && !CheckUrlIsWhiteURL(ctx, clientID, url) {
-				hasURL = true
-			}
-		} else {
-			session.Logger(ctx).Println(err)
-		}
-	} else if msg.Category == mixin.MessageCategoryPlainText {
-		data := tools.Base64Decode(msg.Data)
-		urls := xurls.Relaxed.FindAllString(string(data), -1)
-		for _, url := range urls {
-			if !CheckUrlIsWhiteURL(ctx, clientID, url) {
-				hasURL = true
-				break
-			}
-		}
-	}
-	return hasURL
-}
-
-func checkStickerLimit(ctx context.Context, clientID string, msg *mixin.MessageView) bool {
-	count := 0
-	if err := session.Database(ctx).QueryRow(ctx, `
-SELECT count(1) FROM messages 
-WHERE client_id=$1 AND user_id=$2 AND category=$3
-AND now()-created_at<interval '5 seconds'
-`, clientID, msg.UserID, mixin.MessageCategoryPlainSticker).Scan(&count); err != nil {
-		session.Logger(ctx).Println(err)
-		return false
-	}
-	if count == 2 {
-		go SendStickerLimitMsg(clientID, msg.UserID)
-	}
-	return count >= 5
-}
-
-func checkIsContact(ctx context.Context, clientID, conversationID string) bool {
-	c, err := GetMixinClientByID(ctx, clientID).ReadConversation(ctx, conversationID)
-	if err != nil {
-		session.Logger(ctx).Println(err)
-		return false
-	}
-	if c.Category == mixin.ConversationCategoryContact {
-		return true
-	}
-	return false
-}
-
-func checkCanNotSendLuckyCoin(ctx context.Context, clientID, data, status string) bool {
-	var m mixin.AppCardMessage
-	err := json.Unmarshal(tools.Base64Decode(data), &m)
-	if err != nil {
-		session.Logger(ctx).Println(err)
-		return true
-	}
-	u, err := url.Parse(m.Action)
-	if err != nil {
-		session.Logger(ctx).Println(err)
-		return true
-	}
-	query, _ := url.ParseQuery(u.RawQuery)
-	if len(query["uid"]) == 0 {
-		return true
-	}
-	uid := query["uid"][0]
-	if checkIsBlockUser(ctx, clientID, uid) {
-		return true
-	}
-	user, err := GetClientUserByClientIDAndUserID(ctx, clientID, uid)
-	if err != nil || user == nil || user.UserID == "" {
-		session.Logger(ctx).Println(err, user)
-		return true
-	}
-	if !checkHasClientMemberAuth(ctx, clientID, "lucky_coin", user.Status) {
-		return true
-	}
-	if (status == ClientConversationStatusMute ||
-		status == ClientConversationStatusAudioLive) &&
-		!checkIsAdmin(ctx, clientID, uid) {
-		return true
-	}
-
-	return false
-}
-
-var ignoreMsgList = []string{"Hi", "你好"}
-
-func checkIsIgnoreLeaveMsg(msg *mixin.MessageView) bool {
-	data := string(tools.Base64Decode(msg.Data))
-	for _, s := range ignoreMsgList {
-		if data == s {
-			return true
-		}
-	}
-	return false
-}
-
-func checkMsgLanguage(msg *mixin.MessageView) bool {
-	if msg.Category != mixin.MessageCategoryPlainText {
-		return false
-	}
-	data := string(emojiRx.ReplaceAllString(string(tools.Base64Decode(msg.Data)), ``))
-	if len(data) == 0 {
-		return false
-	}
-	lang := config.Config.Lang
-	return languaueRateCheck(data, lang)
-}
-
-func languaueRateCheck(data, lang string) bool {
-	if lang == "zh" {
-		return false
-	}
-	t := new(unicode.RangeTable)
-	if lang == "en" {
-		t = nil
-	}
-	c, tc := tools.LanguageCount(data, t)
-	return (decimal.NewFromInt(int64(c)).Div(decimal.NewFromInt(int64(tc)))).
-		LessThan(decimal.NewFromInt(2).Div(decimal.NewFromInt(3)))
-}
-
-var forbiddenMsgCategory = map[string]bool{
-	mixin.MessageCategoryPlainAudio:     true,
-	mixin.MessageCategoryPlainLocation:  true,
-	mixin.MessageCategoryAppButtonGroup: true,
-}
-
-func checkMsgIsForbid(u *ClientUser, msg *mixin.MessageView) bool {
-	if forbiddenMsgCategory[msg.Category] {
-		// 发送禁止消息
-		go SendForbidMsg(u.ClientID, u.UserID, msg.Category)
-		return true
-	}
-
-	if msg.Category == mixin.MessageCategoryPlainContact {
-		data := tools.Base64Decode(msg.Data)
-		var c mixin.ContactMessage
-		if err := json.Unmarshal(data, &c); err != nil {
-			return true
-		}
-		contactUser, err := SearchUser(_ctx, c.UserID)
-		if err != nil {
-			return true
-		}
-		id, _ := strconv.Atoi(contactUser.IdentityNumber)
-		if id < 7000000000 {
-			// 联系人卡片消息
-			go SendForbidMsg(u.ClientID, u.UserID, msg.Category)
-			return true
-		}
-	}
-
-	return false
 }
