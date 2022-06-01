9df7bee07a7f54de7845f9179c09689ec7ec09a9
Neo <her_liu@qq.com>
2022-06-01 17:10:42 +0800

    check user is in system bug fixed

--- models/client_user.go
+++ models/client_user.go
@@ -872,11 +872,9 @@ SELECT count(1) FROM client_users WHERE user_id=$1 AND status>1
 }
 
 func checkUserIsInSystem(ctx context.Context, userID string) bool {
-	var count int
-	if err := session.Database(ctx).QueryRow(ctx, `
-SELECT count(1) FROM client_users WHERE user_id=$1
-`, userID).Scan(&count); err != nil {
-		return false
-	}
-	return count > 0
+	var code string
+	err := session.Database(ctx).QueryRow(ctx, `
+SELECT code FROM invitation WHERE user_id=$1
+`, userID).Scan(&code)
+	return errors.Is(err, pgx.ErrNoRows)
 }
