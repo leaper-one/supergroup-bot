1d34840893ae8adb65974e66e56a15c5eaf9adf3
Neo <her_liu@qq.com>
2022-07-02 07:18:05 +0800

    live replay auto play next audio

--- client/src/pages/home/news/liveReplay.tsx
+++ client/src/pages/home/news/liveReplay.tsx
@@ -9,11 +9,14 @@ import { liveReplayPrefixURL } from '@/apis/http';
 import { base64Encode, playlist } from '@/assets/ts/tools';
 import { ApiGetGroup } from '@/apis/group';
 import { $get, $set } from '@/stores/localStorage';
+import { FullLoading } from '@/components/Loading';
 
 export default function Page(props: any) {
   const $t = get$t(useIntl());
   const [live, setLive] = useState<ILive>();
   const [list, setList] = useState<IReplay[]>();
+  const [loading, setLoading] = useState(false);
+  const [loaded, setLoaded] = useState(false);
   const user = $get('_user');
 
   useEffect(() => {
@@ -29,9 +32,17 @@ export default function Page(props: any) {
     }
   }, []);
 
-  const initPage = (id: string) => {
-    ApiGetLiveReplayList(id).then(setList);
-    ApiGetLiveInfo(id).then(setLive);
+  const initPage = async (id: string) => {
+    setLoading(true);
+    const [list, live] = await Promise.all([ApiGetLiveReplayList(id), ApiGetLiveInfo(id)]);
+    setLive(live);
+    let totalRender = Math.ceil(list.length / 20);
+    for (let i = 0; i < totalRender; i++) {
+      setList((prev = []) => [...prev, ...list.slice(i * 20, (i + 1) * 20)]);
+      await new Promise((resolve) => setTimeout(resolve, 100));
+      if (i === 0) setLoading(false);
+    }
+    setLoaded(true);
   };
   const handleClickShared = () => {
     let schema = `mixin://send?category=app_card&data=`;
@@ -48,47 +59,77 @@ export default function Page(props: any) {
 
   const audioList = list && list.filter((item) => item.category === 'PLAIN_AUDIO');
 
-  return (
-    <div className={styles.container}>
-      <BackHeader
-        name={$t('news.liveReplay.title')}
-        action={
-          <>
-            {user && user.status === 9 && <i className={`iconfont iconbar-chart-2 ${styles.stat}`} onClick={() => history.push(`/news/liveStat`)} />}
-            <i className={`iconfont iconic_share ${styles.share}`} onClick={() => handleClickShared()} />
-          </>
+  const action = {
+    onAudioPlay: (idx: number) => {
+      for (let i = 0; i < list!.length; i++) {
+        if (i !== idx && list![i].category === 'PLAIN_AUDIO') {
+          const audioDOM = document.getElementById(`replay-${i}`) as HTMLAudioElement;
+          if (!audioDOM) continue;
+          if (!audioDOM.paused) audioDOM.pause();
         }
-      />
-      <div className={styles.content}>
-        <img src={live?.img_url} alt="" className={styles.main_image} />
-        {list && list.map((item) => msgItem(item))}
+      }
+      const audioDOM = document.getElementById(`replay-${idx}`) as HTMLAudioElement;
+      audioDOM.play();
+    },
+    onAudioEnded: (idx: number) => {
+      let nextIdx = -1;
+      for (let i = idx + 1; i < list!.length; i++) {
+        if (list![i].category === 'PLAIN_AUDIO') {
+          nextIdx = i;
+          break;
+        }
+      }
+      if (nextIdx !== -1) {
+        const audioDom = document.getElementById(`replay-${nextIdx}`) as HTMLAudioElement;
+        audioDom.play();
+      }
+    },
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <BackHeader
+          name={$t('news.liveReplay.title')}
+          action={
+            <>
+              {user && user.status === 9 && <i className={`iconfont iconbar-chart-2 ${styles.stat}`} onClick={() => history.push(`/news/liveStat`)} />}
+              <i className={`iconfont iconic_share ${styles.share}`} onClick={() => handleClickShared()} />
+            </>
+          }
+        />
+        <div className={styles.content}>
+          <img src={live?.img_url} alt="" className={styles.main_image} />
+          {list && list.map((item, idx) => msgItem(item, idx, action))}
+        </div>
+        {audioList && audioList.length >= 2 && loaded && (
+          <img onClick={() => playlist(audioList.map((item) => liveReplayPrefixURL + item.data))} className={styles.backPlay} src={require('@/assets/img/back_play.png')} alt="" />
+        )}
       </div>
-      {audioList && audioList.length >= 2 && (
-        <img onClick={() => playlist(audioList.map((item) => liveReplayPrefixURL + item.data))} className={styles.backPlay} src={require('@/assets/img/back_play.png')} alt="" />
-      )}
-    </div>
+      {loading && <FullLoading mask opacity />}
+    </>
   );
 }
 
-function msgItem(msg: IReplay) {
+function msgItem(msg: IReplay, idx: number, action: any) {
   switch (msg.category) {
     case 'PLAIN_TEXT':
-      return <div className={styles.text} dangerouslySetInnerHTML={{ __html: handleBroadcast(msg.data) }} />;
+      return <div key={idx} className={styles.text} dangerouslySetInnerHTML={{ __html: handleBroadcast(msg.data) }} />;
     case 'PLAIN_AUDIO':
       return (
-        <div className={styles.audio}>
-          <audio src={liveReplayPrefixURL + msg.data} controls />
+        <div key={idx} className={styles.audio}>
+          <audio id={'replay-' + idx} src={liveReplayPrefixURL + msg.data} controls onPlay={() => action.onAudioPlay(idx)} onEnded={() => action.onAudioEnded(idx)} />
         </div>
       );
     case 'PLAIN_IMAGE':
       return (
-        <div className={styles.image}>
+        <div key={idx} className={styles.image}>
           <img src={liveReplayPrefixURL + msg.data} alt="" />
         </div>
       );
     case 'PLAIN_VIDEO':
       return (
-        <div className={styles.video}>
+        <div key={idx} className={styles.video}>
           <video src={liveReplayPrefixURL + msg.data} controls />
         </div>
       );
